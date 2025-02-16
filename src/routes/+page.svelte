3b3a85d42cf1754bd2ed9c9f55f42f13f0f0ac86
Chitwan Singh <119756817+chitwan27@users.noreply.github.com>
2025-02-16 15:35:48 +0530

    daisy ui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,4 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <div class="text-3xl">HOY</div>
+<button class="btn w-64 rounded-full">Button</button>
