7d1e4e0a8deb603a9f555fec9c159476a45c91d0
Chitwan Singh <119756817+chitwan27@users.noreply.github.com>
2025-02-16 15:36:29 +0530

    start

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,3 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <div class="text-3xl">HOY</div>
 <button class="btn w-64 rounded-full">Button</button>
